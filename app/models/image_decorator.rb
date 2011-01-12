29e1e5166e16f98157ed4012569c05ca346cc339
Brian Quinn <briandquinn@gmail.com>
2011-01-12 10:02:43 +0000

    Tweaked sample product data

--- app/models/image_decorator.rb
+++ app/models/image_decorator.rb
@@ -4,7 +4,7 @@ Image.attachment_definitions[:attachment][:s3_credentials] = {
   :secret_access_key => ENV['S3_SECRET']
 }
 Image.attachment_definitions[:attachment][:bucket] = ENV['S3_BUCKET']
-Image.attachment_definitions[:attachment][:path] = ":attachment/:id/:style.:extension"
+Image.attachment_definitions[:attachment][:path] = "products/:id/:style.:extension"
 Image.attachment_definitions[:attachment][:s3_headers] = {'Cache-Control' => 'max-age=31557600'}
 Image.attachment_definitions[:attachment].delete :url
 Image.attachment_definitions[:attachment][:default_style] = :original
