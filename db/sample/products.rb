eafa7776fc694eb030d2c94d648fda0b75fc9541
Brian Quinn <briandquinn@gmail.com>
2011-01-12 09:43:22 +0000

    Added custom product image sample

--- db/sample/products.rb
+++ db/sample/products.rb
@@ -1,15 +1,13 @@
-# make sure the product images directory exists
-FileUtils.mkdir_p "#{Rails.root}/public/assets/products/"
-
-Asset.all.each do |asset|
-  filename = asset.attachment_file_name
+Image.all.each do |image|
+  filename = image.attachment_file_name
   puts "-- Processing image: #{filename}\r"
   path = File.join(File.dirname(__FILE__), "assets/#{filename}")
 
   if FileTest.exists? path
-    asset.attachment = File.open(path)
-    asset.save
+    image.attachment = File.open(path)
+    image.save
   else
     puts "--- Could not find image at: #{path}"
   end
 end
+
